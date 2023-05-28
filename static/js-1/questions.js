fc929e16e8e5f4c58a66448c9de61a24fb33a60b
prathik <20aiml0232@vvce.ac.in>
2023-05-28 08:44:17 +0530

    Initial commit

---
+++ static/js-1/questions.js
@@ -0,0 +1,68 @@
+const questions = [
+  {
+    numb: 1,
+    question: "which of the following is not an SQL command?",
+    answer: "delete",
+    options: [
+      "delete",
+      "order by",
+      "select",
+      "where",
+    ]
+  },
+  {
+    numb: 1,
+    question: "what is the framework used for writing command line applications?",
+    answer: "click",
+    options: [
+      "markupsafe",
+      "jinja",
+      "click",
+      "none of the above",
+    ]
+  },
+  {
+    numb: 1,
+    question: "which of the following datatype is most appropriate for storing string of 255 charecters?",
+    answer: "tiny text",
+    options: [
+      "text",
+      "tiny text",
+      "blob",
+      "binary",
+    ]
+  },
+  {
+    numb: 1,
+    question: "what happens when no value is inserted in an ENUM list?",
+    answer: "blank inserted",
+    options: [
+      "no change",
+      "code crash",
+      "blank inserted",
+      "none of the above",
+    ]
+  },
+  {
+    numb: 1,
+    question: "Identify the correct definition of * operator in pointer.",
+    answer: "value of ",
+    options: [
+      "value of",
+      "star",
+      "not defined",
+      "multiplication",
+    ]
+  },
+  {
+    numb: 1,
+    question: "Which of the following is “address of operator ?",
+    answer: "&",
+    options: [
+      "^",
+      ".",
+      "[]",
+      "&",
+    ]
+  },
+];
